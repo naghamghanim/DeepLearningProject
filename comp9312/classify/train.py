f05427adda4ca7ec73080ec3c65d7a737f1ad01b
naghamghanim <56317559+naghamghanim@users.noreply.github.com>
2022-06-12 23:24:57 +0300

    Add files via upload

--- comp9312/classify/train.py
+++ comp9312/classify/train.py
@@ -57,7 +57,7 @@ def parse_args():
     )
 
     parser.add_argument(
-        "--learning_rate", type=float, default=0.0001, help="Learning rate",
+        "--learning_rate", type=float, default=0.0003, help="Learning rate",
     )
 
     args = parser.parse_args()
